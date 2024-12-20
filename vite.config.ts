a46082e29fbea4f2eb6ec0d1f6ee7a26046a684d
dcruzel <liz.c.dcruz@gmail.com>
2024-12-20 01:39:27 -0500

    update render code

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: './environment',
   plugins: [react()],
+  server: {
+    port: 4000,
+    open: true
+  }
 });
